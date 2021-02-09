47b10c06e9d2b305c6f0ef20195b96efd1081cf9
Zachary Jayes <zacjayes@gmail.com>
2021-02-08 23:56:33 -0600

    committing updated sql db

--- seed.sql
+++ seed.sql
@@ -13,17 +13,20 @@ CREATE TABLE department(
 CREATE TABLE role (
     id INT (100) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(8,2),
+    salary FLOAT(8,2),
     department_id INT (100) NOT NULL,
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE employee (
     id INT(100) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT (100) NOT NULL,
-    manager_id INT (100) NOT NULL,
+    manager_id INT (100) NULL,
     PRIMARY KEY(id)
 );
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Zac", "Jayes", "9", "33");
+INSERT INTO department (name) VALUES ("HR");
+INSERT INTO role (title, salary, department_id) VALUES ("CEO", "100000", "10");
