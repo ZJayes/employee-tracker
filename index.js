123b67ccf11b769a6850bb477744fab4cde7758e
Zachary Jayes <zacjayes@gmail.com>
2021-02-08 22:06:50 -0600

    added array and inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer")
-const mysql = require("mysql")
+const mysql = require("mysql");
+const { exit } = require("process");
+const { formatWithOptions } = require("util");
 
 const connection = mysql.createConnection({
     host     : 'localhost',
@@ -12,4 +14,46 @@ const connection = mysql.createConnection({
     if (err) throw err
     console.log(`Connected to mysql on thread ${connection.threadId}`)
 });
+
+// Add departments, roles, employees
+
+
+const choice = "View all employees, View all departments, View all roles, Add new employee, Add new department, Add new role, update employees role, exit";
+//taking strings and splitting into an array where commas are
+const options = choice.split(",")
+
+
+const employeeTracker = () => {
+    inquirer.prompt([
+        {
+            name:"user",
+            type:"list",
+            message: "What would you like to do?",
+            choices: options,
+        }
+    ]).then(answer => {
+        if(answer.action === options[0]){
+            viewEmployees()
+        }else if (answer.action === options [1]){
+            viewDepartments()
+        }else if (answer.action === options [2]){
+            viewRoles()
+        }else if (answer.action === options [3]){
+            addEmployee()
+        }else if (answer.action === options [4]){
+            addDepartment()
+        }else if (answer.action === options [5]){
+            addRole()
+        }else if (answer.action === options [6]){
+            updateRole()
+        }else {
+            exit()
+        }
+    })
+}
+
+// View departments, roles, employees
+
+
+// Update employee roles
 
